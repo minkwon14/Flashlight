e973d8bd3b60d0ead39ab1f377791bb9595fab3a
minkwon14 <min.kwon14@gmail.com>
2017-11-29 01:17:31 -0500

    Finished 171129 0117

--- Flashlight/ViewController.swift
+++ Flashlight/ViewController.swift
@@ -10,93 +10,101 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
+    
+    // Declaring IBOutlets
     @IBOutlet weak var onBtn: UIButton!
     @IBOutlet weak var powSlider: UISlider!
     @IBOutlet weak var timeSlider: UISlider!
     
-    
+    // Declaring all of variables
     var clickCheck = false
     var powerCheck = true
     var timeCheck = false
     var currentPower: Float = 1.0
     var currentTime: Double = 0.0
-    var timer: Timer?
+    var delayTime: Double = 0.0
+    var blinkTimer = Timer()
+    var offTimer = Timer()
+    var onTimer = Timer()
 
     let device = AVCaptureDevice.default(for: AVMediaType.video)
     var image01 = UIImage(named: "powerBtn01.png")
     var image02 = UIImage(named: "powerBtn02.png")
     
+    // IBAction for on Button.
     @IBAction func modeOn(_ sender: Any) {
         if clickCheck == false {
-            timer?.invalidate()
             clickCheck = true
-            alwaysOn()
+            // Change image when taped.
             self.onBtn.setImage(image02, for: .normal)
+            alwaysOn()
         } else {
-            timer?.invalidate()
             clickCheck = false
-            alwaysOff()
             self.onBtn.setImage(image01, for: .normal)
+            blinkTimer.invalidate()
+            offTimer.invalidate()
+            onTimer.invalidate()
+            alwaysOff()
         }
     }
     
+    // IBAction of power slider.
     @IBAction func powSliderValue(_ sender: UISlider) {
         currentPower = sender.value
         powerCheck = true
-        if clickCheck == true {
+        if ( clickCheck == true){
             alwaysOn()
         }
     }
     
+    // IBAction of tiem slider.
     @IBAction func timeSliderValue(_ sender: UISlider) {
-        currentTime = Double (sender.value)
+        // Get and set time from Float to Double
+        currentTime = Double (round(10*sender.value)/100)
+        delayTime = currentTime
+        blinkTimer.invalidate()
+
         if (currentTime == 0.0){
             timeCheck = false
+            if clickCheck == true {
+                alwaysOn()
+            }
         } else {
             timeCheck = true
             if clickCheck == true {
-                
+                // When time has set, make torch blink with set time.
+                // Timer has been used with time inverval of currentTime.
+                blinkTimer = Timer.scheduledTimer(timeInterval: currentTime, target: self, selector: #selector(ViewController.blink), userInfo: nil, repeats: true)
             } else {
-                timer?.invalidate()
                 alwaysOff()
             }
         }
     }
-    
-    
-    @objc func blinkOn() -> Void {
-        
+
+    // Obj-C function that torch blink.
+    @objc func blink() {
+        offTimer.invalidate()
+        onTimer.invalidate()
         if (device?.hasTorch)! {
             do {
                 try device?.lockForConfiguration()
-                
-                 //alwaysOn()
-                
-                
-                Timer.scheduledTimer(timeInterval: currentTime, target: self, selector: #selector(ViewController.alwaysOn), userInfo: nil, repeats: false)
-                
-                
-                Timer.scheduledTimer(timeInterval: currentTime, target: self, selector: #selector(ViewController.alwaysOff), userInfo: nil, repeats: false)
-                
-                
-                //alwaysOff()
-                
+                alwaysOff()
+                blinkOn()
                 device?.unlockForConfiguration()
             } catch {
                 print ("E.blicnkOn: Torch could not be used!!")
             }
-            
         } else {
             print ("E.blickOn: Torch is not available!")
         }
-        
-    
     }
     
-    @objc func alwaysOn() -> Void {
+    // Obj-C function that torch be always on.
+    @objc func alwaysOn() {
         if (device?.hasTorch)! {
             do {
                 try device?.lockForConfiguration()
+                // Torch will be on with currentPower.
                 try! device?.setTorchModeOn(level: currentPower)
                 device?.unlockForConfiguration()
             } catch {
@@ -106,8 +114,8 @@ class ViewController: UIViewController {
             print ("E.alwaysOn: Torch is not available!")
         }
     }
-    
 
+    // Obj-C funtion that torch be always off.
     @objc func alwaysOff () {
         if (device?.hasTorch)! {
             do {
@@ -122,6 +130,13 @@ class ViewController: UIViewController {
         }
     }
     
+    // Function that make torch blink with Timer.
+    func blinkOn() {
+        
+        onTimer = Timer.scheduledTimer(timeInterval: delayTime/2, target: self, selector: #selector(ViewController.alwaysOn), userInfo: nil, repeats: false)
+        
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -131,45 +146,6 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    
-    func toggleTorch(on: Bool, power: Bool, time: Bool) {
-        if (device?.hasTorch)! {
-            do {
-                try device?.lockForConfiguration()
-                
-                if on == true {
-                    
-                    if (power == true && time == true) {
-                        
-                        timer = Timer.scheduledTimer(timeInterval: currentTime, target: self, selector: #selector(ViewController.blinkOn), userInfo: nil, repeats: true)
-                        
-                        print ("Timer needs to be fixed!!")
-                        
-                    } else if (power == true && time == false){
-                        
-                        try! device?.setTorchModeOn(level: currentPower)
-                        // device.torchMode = .on
-                        print ("Torch is ON!!")
-                        
-                    } else {
-                        
-                    }
-                } else {
-                    device?.torchMode = .off
-                    timer?.invalidate()
-                    print ("Torch is OFF!!")
-                }
-                device?.unlockForConfiguration()
-                
-            } catch {
-                print ("Torch could not be used!!")
-            }
-            
-        } else {
-            print ("Torch is not available!")
-        }
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle{
